4451c74b96bd2d358b25bb63535fb3e54503d3d8
Gian Tran <gianqng97@gmail.com>
2019-06-16 05:43:42 +0700

    Restructuring web client, add login, recover password page.

---
+++ WebClient/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("dang-nhap")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("dat-lai-mat-khau")]
+        public IActionResult RecoverPassword()
+        {
+            return View();
+        }
+
+
+
+    }
+}
