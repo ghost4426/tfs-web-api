296c44741dbbdcb8afa4c433877b5b7a31c2dad1
Nhat Anh <nhatanh2321997@gmail.com>
2019-06-19 14:54:53 +0700

    change port

--- WebClient/wwwroot/js/provider/provider.js
+++ WebClient/wwwroot/js/provider/provider.js
@@ -5,12 +5,12 @@
 function testAPI() {
     var request = new XMLHttpRequest();
 
-    request.open('GET', 'http://localhost/api/food', true);
+    request.open('GET', 'https://localhost:4201/getByProvider?providerID=2', true);
     request.onload = function () {
         var data = JSON.parse(this.response);
 
         if (request.status >= 200 && request.status < 400) {
-            console.log(data[0].id);
+            console.log(data);
         } else {
             console.log("error");
         }
