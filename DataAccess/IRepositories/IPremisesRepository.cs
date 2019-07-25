b935980df9b2ecda4f54fd18945a8c1d7690e828
thaihd <thaihdse61760@fpt.edu.vn>
2019-07-25 17:13:47 +0700

    add Find By Name Premises

--- DataAccess/IRepositories/IPremisesRepository.cs
+++ DataAccess/IRepositories/IPremisesRepository.cs
@@ -1,13 +1,17 @@
 ﻿using DTO.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DataAccess.IRepositories
 {
-    public interface IPremisesRepository: IGenericRepository<Premises>
+    public interface IPremisesRepository : IGenericRepository<Premises>
     {
+        Task<Premises> FindByName(string premisesName);
+
         Task<IList<Premises>> getAllProviderAsync(string keyword);
     }
 }
